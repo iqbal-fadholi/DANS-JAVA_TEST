7ee4faed0f0d61ece0cb17c5709a4c8c2e42f275
mifadholi <mifadholi@bsm.co.id>
2022-11-19 15:19:20 +0700

    update rest implementation

--- src/main/java/com/dansmulticorp/service/JobResourceServiceRestImpl.java
+++ src/main/java/com/dansmulticorp/service/JobResourceServiceRestImpl.java
@@ -8,7 +8,6 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.NoSuchElementException;
 
 @Slf4j
 @Service
@@ -18,39 +17,26 @@ public class JobResourceServiceRestImpl {
     private String jpPositionUrl;
 
 
-    public List<DansJobResourceResponseModel> getAllJobPosition(){
-        return getDansJobPositionResource();
+    public List<DansJobResourceResponseModel> getAllJobPosition() {
+        RestTemplate restTemplate = new RestTemplate();
+
+        log.debug("request job position data. ");
+
+        return Arrays.asList(
+                restTemplate.getForObject(jpPositionUrl + "/positions.json", DansJobResourceResponseModel[].class)
+        );
     }
 
 
-    public DansJobResourceResponseModel getJobPosition(String id){
-
-        List<DansJobResourceResponseModel> dansJobResourceResponseModelList = getDansJobPositionResource();
-
-        if (!dansJobResourceResponseModelList.isEmpty()){
-            for (DansJobResourceResponseModel dansJobResourceResponseModel: dansJobResourceResponseModelList) {
-                if (dansJobResourceResponseModel.getId().equalsIgnoreCase(id)) return dansJobResourceResponseModel;
-                else continue;
-            }
-            throw new NoSuchElementException("id " + id + " not found");
-        }else throw new NoSuchElementException("Job position List is empty");
-
-    }
-
-    private List<DansJobResourceResponseModel> getDansJobPositionResource() {
-        DansJobResourceResponseModel[] result;
-
-//        RestTemplate restTemplate = createResttemplateWithTimeout(timeout);
+    public DansJobResourceResponseModel getJobPosition(String id) {
 
         RestTemplate restTemplate = new RestTemplate();
 
         log.debug("request job position data. ");
 
-        result = restTemplate.getForObject(jpPositionUrl, DansJobResourceResponseModel[].class);
+        return restTemplate.getForObject(jpPositionUrl + "/positions/" + id, DansJobResourceResponseModel.class);
 
-        log.debug("Dans job position data response : " + result.toString());
-
-        return Arrays.asList(result);
     }
+
 
 }
